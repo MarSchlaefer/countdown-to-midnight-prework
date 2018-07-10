689b646a1dae4873b646f61cea69333a74bffcff
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-07-10 19:42:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 
 
 def countdown(integer)
+  counter
   while integer > 0 do
     puts "#{integer} SECOND(S)!"
     integer -= 1
   end
   if integer == 0 do
     puts "HAPPY NEW YEAR!"
-  end
 end
